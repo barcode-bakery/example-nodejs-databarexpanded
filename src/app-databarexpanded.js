5c76a816837c7616672fd82f3437f3544168a347
Jean-Sebastien Goupil <contact@jsgoupil.com>
2021-05-19 12:13:19 -0700

    Initial commit.

---
+++ src/app-databarexpanded.js
@@ -0,0 +1,55 @@
+﻿import { createServer } from 'http';
+import { parse } from 'querystring';
+import { BCGColor, BCGDrawing, BCGFont } from '@barcode-bakery/barcode-common';
+import { BCGdatabarexpanded } from '@barcode-bakery/barcode-databarexpanded';
+
+let defaultText = '01900123456789083103001750';
+
+// Loading Font
+let font = new BCGFont('Arial', 18);
+
+// The arguments are R, G, B for color.
+let colorBlack = new BCGColor(0, 0, 0);
+let colorWhite = new BCGColor(255, 255, 255);
+
+let getDrawing = function (text) {
+    let drawException = null,
+        barcode = null;
+    try {
+        let code = new BCGdatabarexpanded();
+        code.setScale(1);
+        code.setStacked(1);
+        code.setBackgroundColor(colorWhite); // Color of spaces
+        code.setForegroundColor(colorBlack); // Color of bars
+        code.setFont(font);
+        code.setLinkageFlag(false);
+        code.parse(text); // Text
+        barcode = code;
+    } catch (exception) {
+        drawException = exception;
+    }
+
+    let drawing = new BCGDrawing(barcode, colorWhite);
+    if (drawException) {
+        drawing.drawException(drawException);
+    }
+
+    return drawing;
+};
+
+/*
+// This is how you would save to a file.
+let drawing = getDrawing(defaultText);
+drawing.save("image.png", BCGDrawing.ImageFormat.Png, function () {
+    console.log("Done.");
+});
+*/
+
+createServer(function (request, response) {
+    let drawing = getDrawing(parse(request.url).query?.toString() || defaultText);
+    drawing.toBuffer(BCGDrawing.ImageFormat.Png, function (err, buffer) {
+        response.writeHead(200, { 'Content-Type': 'image/png' });
+        response.end(buffer);
+    });
+}).listen(8124);
+console.log('Server running at http://127.0.0.1:8124/');
